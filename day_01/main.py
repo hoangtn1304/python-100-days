e4de97a3cf497a36aaaa19d3b12636c212b0e1b2
Hoang N. Tran <hoangtn1304@gmail.com>
2024-11-06 22:18:25 +0000

    code day 1

---
+++ day_01/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+
+answer_question_1 = input("What's the name of the city you grew up in?\n")
+answer_question_2 = input("What's your pet's name?\n")
+
+print(f"Your band name could be {answer_question_1} {answer_question_2}")
